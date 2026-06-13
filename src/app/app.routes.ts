333b27d4ba63a325dd8822080214bcce97775018
fatima doughman <fatimadoughman710@gmail.com>
2026-06-13 22:24:03 +0300

    homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AdminLogin } from './admin-login/admin-login';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from '../layout/homepage/homepage.component';
 export const routes: Routes = [
 {
   path: 'admin-login',
@@ -9,5 +11,10 @@ export const routes: Routes = [
 {
   path: 'admin-dashboard',
   component: AdminDashboardComponent
+},
+
+{
+  path: '',
+  component: HomepageComponent
 }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminLogin } from "./admin-login/admin-login";
+import { NavbarComponent } from "./navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
