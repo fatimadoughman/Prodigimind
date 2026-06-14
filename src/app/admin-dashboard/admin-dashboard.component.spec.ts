70254dcca37401fc814e8ef96c90ee95bc74a0e4
prodigimindd <prodigimindd@gmail.com>
2026-06-14 19:51:05 +0300

    yl3an byak

--- src/app/admin-dashboard/admin-dashboard.component.spec.ts
+++ src/app/admin-dashboard/admin-dashboard.component.spec.ts
@@ -1,28 +1,56 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
-import { AdminDashboardComponent } from './admin-dashboard.component';
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css']
+})
+export class AdminDashboardComponent {
 
-describe('AdminDashboardComponent', () => {
-  let component: AdminDashboardComponent;
-  let fixture: ComponentFixture<AdminDashboardComponent>;
+  showCard = false;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminDashboardComponent ]
-    })
-    .compileComponents();
-  }));
+  newCourse = {
+    title: '',
+    image: '',
+    description: ''
+  };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminDashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  openCard() {
+    this.showCard = true;
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  onImageUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.newCourse.image = reader.result as string;
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  addCourse() {
+    console.log(this.newCourse);
+
+    alert('Course Added Successfully');
+
+    this.newCourse = {
+      title: '',
+      image: '',
+      description: ''
+    };
+
+    this.showCard = false;
+  }
+}
