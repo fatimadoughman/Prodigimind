9692de868dc6f20212bdb03950919bec73afe188
fatima doughman <fatimadoughman710@gmail.com>
2026-06-16 16:07:40 +0300

    all

--- src/app/servicess/servicess.component.ts
+++ src/app/servicess/servicess.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-servicess',
   templateUrl: './servicess.component.html',
-  styleUrls: ['./servicess.component.css']
+  styleUrls: ['./servicess.component.css'],
+
 })
 export class ServicessComponent implements OnInit {
 
