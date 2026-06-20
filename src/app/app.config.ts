bc19dff27e83804fabae1bf2238c65a6105bded7
fatima doughman <fatimadoughman710@gmail.com>
2026-06-20 15:33:23 +0300

    app-route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+// import { provideRouter, withInMemoryScrolling } from '@angular/router';
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideRouter(routes, withInMemoryScrolling({
+//       anchorScrolling: 'enabled',
+//       scrollPositionRestoration: 'enabled'
+//     }))
+//   ]
+// };
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withInMemoryScrolling({
-      anchorScrolling: 'enabled',
-      scrollPositionRestoration: 'enabled'
-    }))
+    provideRouter(routes, withHashLocation())
   ]
 };
